06520b18a88b66965621f02a13f94d435453e07a
Devi R <devi2357@gmail.com>
2025-09-30 18:10:33 +0530

    fix: correct case for PostLoginDashboard.js filename

--- src/pages/PostLoginDashboard.js
+++ src/pages/PostLoginDashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import DashboardWrapper from "../components/dashboardWrapper/DashboardWrapper";
 import { ConfigProvider } from "../contexts/ConfigContext";
 
+// PostLoginDashboard
 function PostLoginDashboard() {
   return (
     <ConfigProvider>
